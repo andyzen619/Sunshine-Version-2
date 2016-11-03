02e6585d79e085b71d23f7c4051674fe19eea9e0
Andy <andyzen619@gmail.com>
2016-11-03 17:50:48 -0400

    Started on JsonWeatherExtractor class to extract json string data

--- app/src/main/java/com/example/android/sunshine/app/JsonWeatherExtractor.java
+++ app/src/main/java/com/example/android/sunshine/app/JsonWeatherExtractor.java
@@ -1,6 +1,13 @@
 package com.example.android.sunshine.app;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Andy DESK on 11/2/2016.
@@ -8,9 +15,12 @@ import org.json.JSONException;
 
 public class JsonWeatherExtractor {
 
-    //TODO:1
+
     private String getReadableDateString(long time){
-        return "";
+        String dateFormat = "dd MM yyyy";
+        Date date = new Date(time);
+        SimpleDateFormat format = new SimpleDateFormat(dateFormat);
+        return format.format(date);
     }
 
     //TODO:2
@@ -18,7 +28,8 @@ public class JsonWeatherExtractor {
      * Prepare the weather high/lows for presentation.
      */
     private String formatHighLows(double high, double low) {
-        return "";
+        String result = "High: " + high + " Low: " + low;
+        return result;
     }
 
     //TODO:3
@@ -31,6 +42,29 @@ public class JsonWeatherExtractor {
      */
     private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
             throws JSONException {
+
+        String dayList = "list";
+        String temperature = "temp";
+        String maxTemp = "max";
+        String minTemp = "min";
+        String description = "main";
+        String weatherAtt = "weather";
+
+        JSONObject baseJsonString = new JSONObject(forecastJsonStr);
+        JSONArray listOfDays = baseJsonString.getJSONArray(dayList);
+
+        Date date = new Date();
+        Long currentTime = date.getTime();
+
+
+        for(int i = 0; i < listOfDays.length(); i ++) {
+            JSONObject day = (JSONObject) listOfDays.get(i);
+            JSONObject temp = day.getJSONObject(temperature);
+            Double max = temp.getDouble(maxTemp);
+            Double min = temp.getDouble(minTemp);
+            JSONObject weather = day.getJSONObject(weatherAtt);
+            String weatherDescription = weather.getString(description);
+        }
         return null;
     }
 }
