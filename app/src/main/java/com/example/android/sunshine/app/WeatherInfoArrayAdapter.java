d52b0ae15726abb5aa560cc0a4402a0b6730bf92
Andy <andyzen619@gmail.com>
2016-11-15 18:03:47 -0500

    stuff

--- app/src/main/java/com/example/android/sunshine/app/WeatherInfoArrayAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherInfoArrayAdapter.java
@@ -1,8 +1,14 @@
 package com.example.android.sunshine.app;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,8 +17,24 @@ import java.util.List;
 
 public class WeatherInfoArrayAdapter extends ArrayAdapter {
 
+    private ArrayList<WeatherInfo> weatherInfoCollection;
+
 
     public WeatherInfoArrayAdapter(Context context, int resource, int textViewResourceId, List objects) {
         super(context, resource, textViewResourceId, objects);
+    }
+
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        WeatherInfo selectedWeatherInfo = (WeatherInfo) getItem(position);
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_forcast, parent, false);
+        }
+        TextView mainFragmentWeatherText = (TextView) convertView.findViewById(R.id.list_item_forcast_textview);
+        mainFragmentWeatherText.setText(selectedWeatherInfo.mainFormat());
+        return convertView;
     }
 }
